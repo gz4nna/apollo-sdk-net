1639b0830d3593a7cc9b411bc141c6de5c0ca97f
gz4nna <gz4nna@gmail.com>
2026-01-12 12:42:29 +0800

    操作符测试

---
+++ Apollo.SDK.DotNet.Tests/CustomAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using Apollo.SDK.DotNet.Models;
+
+namespace Apollo.SDK.DotNet.Tests;
+
+public class CustomAttributeTests
+{
+    private readonly RuleEvaluator _evaluator = new();
+
+    [Fact]
+    public void Custom_Attribute_Should_Work()
+    {
+        var rule = new Rule
+        {
+            Attribute = "custom",
+            CustomAttribute = "vip_level",
+            Operator = "equals",
+            Value = "9"
+        };
+        var context = new Dictionary<string, object> { { "vip_level", "9" } };
+
+        Assert.True(_evaluator.Evaluate(rule, context));
+    }
+}
---
+++ Apollo.SDK.DotNet.Tests/OperatorTests.cs
@@ -0,0 +1,58 @@
+﻿using Apollo.SDK.DotNet.Models;
+
+namespace Apollo.SDK.DotNet.Tests;
+
+public class OperatorTests
+{
+    private readonly RuleEvaluator _evaluator = new();
+
+    [Theory]
+    #region equals
+    [InlineData("equals", "100", "100", true)]
+    [InlineData("equals", "100", "101", false)]
+    #endregion
+
+    #region not_equals
+    [InlineData("not_equals", "A", "B", true)]
+    [InlineData("not_equals", "A", "A", false)]
+    #endregion
+
+    #region contains
+    [InlineData("contains", "apple", "I like apple", true)]
+    #endregion
+
+    #region in
+    [InlineData("in", "Beijing,Shanghai,Guangzhou", "Beijing", true)]
+    [InlineData("in", "Beijing,Shanghai", "Tokyo", false)]
+    #endregion
+
+    #region gt
+    [InlineData("gt", "30", 20, false)]
+    [InlineData("gt", "30", 30, false)]
+    [InlineData("gt", "30", 40, true)]
+    #endregion
+
+    #region lt
+    [InlineData("lt", "30", 20, true)]
+    [InlineData("lt", "30", 30, false)]
+    [InlineData("lt", "30", 40, false)]
+    #endregion
+
+    #region between
+    [InlineData("between", "0,10", 5, true)]
+    [InlineData("between", "0,10", 0, true)]
+    [InlineData("between", "0,10", 10, true)]
+    [InlineData("between", "0,10", 11, false)]
+    [InlineData("between", "0,10", -1, false)]
+    #endregion
+
+    public void All_Base_Operators_Should_Work(string op, string configVal, object userVal, bool expected)
+    {
+        var rule = new Rule { Operator = op, Value = configVal, Attribute = "test_key" };
+        var context = new Dictionary<string, object> { { "test_key", userVal } };
+
+        var result = _evaluator.Evaluate(rule, context);
+
+        Assert.Equal(expected, result);
+    }
+}
