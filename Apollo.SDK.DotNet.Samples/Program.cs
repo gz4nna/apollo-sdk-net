18be7a853a1cafed32e40a53bfc1c44b3773cb5f
gz4nna <gz4nna@gmail.com>
2026-01-14 23:22:07 +0800

    添加示例项目

---
+++ Apollo.SDK.DotNet.Samples/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Apollo.SDK.DotNet.Samples;
+
+public class Program
+{
+    public static void Main()
+    {
+        ApolloClient client = new();
+
+        var path = Path.Combine(Environment.CurrentDirectory, "toggles");
+        client.SetTogglesPath(path);
+
+        var user_id = "user_123";
+
+        var context = new Dictionary<string, object>
+        {
+            { "user_id", user_id},
+            { "city", "Beijing"}
+        };
+
+        if (client.IsToggleAllow("smart_recommender_v2", user_id, context))
+        {
+            Console.WriteLine("is allow");
+        }
+    }
+}
+
+
