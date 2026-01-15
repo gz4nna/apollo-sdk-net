e7afcb025d4ecc304e5a3799cb6b163621a2f11a
gz4nna <gz4nna@gmail.com>
2026-01-15 15:59:52 +0800

    支持依赖注入

---
+++ Apollo.SDK.DotNet/ApolloServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Apollo.SDK.DotNet;
+using Apollo.SDK.DotNet.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// 提供 Apollo 服务的扩展方法，用于将 Apollo 客户端添加到依赖注入容器中。
+/// </summary>
+public static class ApolloServiceCollectionExtensions
+{
+    public static IServiceCollection AddApollo(this IServiceCollection services, Action<ApolloOptions> configureOptions)
+    {
+        var options = new ApolloOptions();
+        configureOptions(options);
+
+        _ = services.AddSingleton<IApolloClient>(serviceProvider => new ApolloClient(options));
+
+        return services;
+    }
+}
